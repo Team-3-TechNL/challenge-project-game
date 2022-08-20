84070a0ab47f9a45655cfda051f5f48d9ec7a71b
DipStudios <dipayan.dhar81@gmail.com>
2022-08-20 14:27:02 -0230

    Implement animation into player movement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame, sys
+import animations
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -8,7 +9,7 @@ screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("Movement Test")
-walk_sprite_sheet = pygame.image.load('images/king_walk_test.png').convert_alpha()
+
 
 # Jump Settings
 jumping = False
@@ -17,13 +18,36 @@ jump_height = 30  # CHANGE THIS VARIABLE TO CHANGE JUMP HEIGHT
 playerY_speed = jump_height
 playerX_speed = 0
 
-player = walk_sprite_sheet.get_rect()
 
-# Initial position
-player.bottomleft = (0, screen_height-1)
 
 # Rect Objects
 floor = pygame.Rect(1, screen_height, screen_width, 1)
+
+player = animations.crowned_walking_frames[0].get_rect()
+# Initial position
+player.bottomleft = (0, screen_height-1)
+
+
+def animate():
+    global player
+    animations.frame_time += 1 # increases every 1/60 of a second
+    if animations.frame_time > 60:
+        animations.frame_time = 0 # resets every second
+
+    if animations.frame_time % 3 == 0:  # player moving at 20 FPS (60 / 3 = 20)
+        # detects key presses
+        if keys[pygame.K_RIGHT]:
+            animations.active_king += 1
+        if keys[pygame.K_LEFT]:
+            animations.active_king -= 1
+        # changes between right/left
+        if animations.active_king >= 8:
+            animations.active_king = 0
+        if animations.active_king <= -8:
+            animations.active_king = -1
+
+    # places frames onto player object
+    screen.blit(animations.crowned_walking_frames[animations.active_king], player)
 
 
 def draw_rects():
@@ -70,16 +94,17 @@ while True:
             if event.key == pygame.K_LEFT:
                 playerX_speed = 0
 
+    keys = pygame.key.get_pressed()
+
+
     player.x += playerX_speed
 
-    screen.fill("black")
-
-    screen.blit(walk_sprite_sheet, player)
+    screen.fill("grey")
 
     draw_rects()
     collisions()
     jump()
-
+    animate()
 
     clock.tick(60)
     pygame.display.flip()
